5d08b54cf8785ca1eb6c0eca3bbd2217fed8cf9a
Shimpei Fukuya <fukuyan03@fukuyan.local>
2026-03-28 23:25:15 +0900

    ShopIndex

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "shops/index"
   root "home#top"
 
   get    "/login",  to: "sessions#new"
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resource :users, only: %i[new create show]
+  resources :shops, only: %i[index new create update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
