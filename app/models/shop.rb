e8acbd5d2041c247060a66ccc5d6d355e3da47cd
Shimpei Fukuya <fukuyan03@fukuyan.local>
2026-03-30 01:27:14 +0900

    ItemModelの作成

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  has_many :items, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :name, uniqueness: { scope: :user_id, case_sensitive: false }
