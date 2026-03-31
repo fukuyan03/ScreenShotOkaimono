c69f6707fcc6ce4197452c4af9429b68e884e7be
Shimpei Fukuya <fukuyan03@fukuyan.local>
2026-03-31 15:42:39 +0900

    Itemsの登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,27 @@ class ItemsController < ApplicationController
     @shop = current_user.shops.find(params[:shop_id])
     @items = @shop.items
   end
+
+  def new
+    @shop = current_user.shops.find(params[:shop_id])
+    @item = @shop.items.build
+  end
+
+  def create
+    @shop = current_user.shops.find(params[:shop_id])
+    @item = @shop.items.build(item_params)
+
+    if @item.save
+      redirect_to shop_items_path(@shop), notice: t("flash.item.create.success")
+    else
+      flash.now[:alert] = t("flash.item.create.failure")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :brand, :price, :sammary, :source_platform, :source_author_name, :status)
+  end
 end
